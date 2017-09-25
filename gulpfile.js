1e692babf145cc621232b1e58590a76622879f72
aleksandrmyronov <a.myronow@gmail.com>
2017-09-25 10:36:27 +0300

    add style

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const pug = require('gulp-pug');
 
 gulp.task('pug', () => {
     "use strict";
-    gulp.src('src/pug/**/*.pug')
+    gulp.src('src/pug/.pug')
         .pipe(pug({
             pretty: '\t'
         }))
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('dist/html'))
 });
 
 gulp.task('watch', () => {
     "use strict";
-    gulp.watch('src/pug/**/*.pug', ['pug'])
-});
+    gulp.watch('src/pug/.pug', ['pug'])
+});pug --watch --pretty ./src/pug/ --out ./dist/html/
